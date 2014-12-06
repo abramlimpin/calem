3ec313fc7d824e67b4941520e47da84766c5048c
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2014-12-06 14:44:49 +0800

    Display User Info

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,15 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            GetCurrentLocation();
+        }
+    }
 
+    void GetCurrentLocation()
+    {
+        Session["latitude"] = "14.5588542";
+        Session["longitude"] = "121.0174931";
     }
 }
